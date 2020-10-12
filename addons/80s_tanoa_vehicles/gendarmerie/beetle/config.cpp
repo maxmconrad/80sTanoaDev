967a7967eae898bb528cf3692aefb47c4016643e
Max-Morten Conrad <max@kokoni.eu>
2020-10-12 18:55:31 +0200

    - Gendarmerie: Beetle inventory

--- addons/80s_tanoa_vehicles/gendarmerie/beetle/config.cpp
+++ addons/80s_tanoa_vehicles/gendarmerie/beetle/config.cpp
@@ -82,6 +82,24 @@ class CfgVehicles
 		scopeCurator=2;
         scopeArsenal=2;
 		author="quarren";
+        class TransportItems
+        {
+            class _xx_burnBandage
+            {
+                name="gm_ge_army_burnBandage";
+                count=2;
+            };
+            class _xx_gauzeBandage
+            {
+                name="gm_ge_army_gauzeBandage";
+                count=2;
+            };
+            class _xx_gm_repairkit_01
+            {
+                name="gm_repairkit_01";
+                count=1;
+            };
+        };
 		hiddenSelectionsTextures[]=
 		{
 			"\gm\gm_vehicles\gm_land\gm_wheeled\gm_typ1\gm_ge_civ_typ1\data\col\gm_ge_civ_typ1_int_01_saphireblue_co.paa",
