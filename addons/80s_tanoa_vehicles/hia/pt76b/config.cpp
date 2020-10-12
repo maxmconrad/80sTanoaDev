71619a69a48e7cd4b91c1475750757f3ccd0215c
Max-Morten Conrad <max@kokoni.eu>
2020-10-12 18:45:08 +0200

    - HIA: PT-76B inventory

--- addons/80s_tanoa_vehicles/hia/pt76b/config.cpp
+++ addons/80s_tanoa_vehicles/hia/pt76b/config.cpp
@@ -74,6 +74,50 @@ class CfgVehicles
 		scopeCurator=2;
         scopeArsenal=2;
 		author="quarren";
+        class TransportItems
+        {
+            class _xx_burnBandage
+            {
+                name="gm_ge_army_burnBandage";
+                count=4;
+            };
+            class _xx_gauzeBandage
+            {
+                name="gm_ge_army_gauzeBandage";
+                count=4;
+            };
+            class _xx_gm_repairkit_01
+            {
+                name="gm_repairkit_01";
+                count=1;
+            };
+        };
+        class TransportWeapons
+        {
+            class _xx_binoculars
+            {
+                weapon="gm_df7x40_grn";
+                count=1;
+            };
+            class _xx_flare_pistol
+            {
+                weapon="gm_p2a1_blk";
+                count=1;
+            };
+        };
+        class TransportMagazines
+        {
+            class _xx_gm_1Rnd_265mm_flare_multi_red_DM23
+            {
+                magazine="gm_1Rnd_265mm_flare_multi_red_DM23";
+                count=4;
+            };
+            class _xx_gm_handGrenade_frag_rgd5
+            {
+                magazine="gm_handGrenade_frag_rgd5";
+                count=4;
+            };
+        };
 		hiddenSelectionsTextures[]=
 		{
 			"#(rgb,8,8,3)color(0.199,0.230,0.171,1,CO)",
