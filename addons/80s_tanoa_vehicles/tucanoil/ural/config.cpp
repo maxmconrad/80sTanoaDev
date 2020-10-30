08e66d46a072b8409560b771b7ad339188ad499c
Max-Morten Conrad <max@kokoni.eu>
2020-10-30 19:53:13 +0100

    - Tucan Oil: Ural Cfg & retexture

---
+++ addons/80s_tanoa_vehicles/tucanoil/ural/config.cpp
@@ -0,0 +1,127 @@
+class CfgPatches
+{
+	class 80s_tanoa_vehicles_tucanoil_ural
+	{
+		units[]=
+        {
+            "80s_tanoa_tucanoil_ural_refuel",
+            "80s_tanoa_tucanoil_civ_ural_refuel"
+        };
+		weapons[]=
+        {
+        };
+		requiredAddons[]=
+		{
+			"gm_core_vehicles"
+		};
+	};
+};
+
+/////////////////////////
+// Ural (Fuel)          /
+/////////////////////////
+class gm_texturesource_base;
+class CfgVehicles
+{
+    class class gm_ural375d_base;
+	class class gm_ural375d_refuel_base: gm_ural375d_base
+	{
+        class TextureSources
+		{
+			class 80s_tanoa_tucanoil_ylw: gm_texturesource_base
+			{
+				factions[]=
+				{
+					"80s_tanoa_fc_tucanoil"
+				};
+				displayname="Tucan Oil (Yellow)";
+				textures[]=
+				{
+					"#(rgb,8,8,3)color(0.25,0.25,0.25,1,CO)",
+					"\80s_tanoa\80s_tanoa_vehicles\tucanoil\ural\data\80s_tanoa_tucanoil_ural_refuel_ext_01_co.paa",
+                    "\80s_tanoa\80s_tanoa_vehicles\tucanoil\ural\data\80s_tanoa_tucanoil_ural_refuel_ext_01_co.paa",
+                    "\80s_tanoa\80s_tanoa_vehicles\tucanoil\ural\data\80s_tanoa_tucanoil_ural_refuel_ext_03_co.paa",
+                    "\80s_tanoa\80s_tanoa_vehicles\tucanoil\ural\data\80s_tanoa_tucanoil_ural_refuel_ext_01_co.paa",
+                    "\80s_tanoa\80s_tanoa_vehicles\tucanoil\ural\data\80s_tanoa_tucanoil_ural_refuel_ext_01_co.paa"
+				};
+			};
+		};
+	};
+	class 80s_tanoa_tucanoil_ural_refuel_base: gm_ural375d_refuel_base
+	{
+		side=2;
+		faction="80s_tanoa_fc_tucanoil";
+		gm_InsigniasDefaultNation="gm_insignia_none";
+		gm_LicensePlate="80s_tanoa_licenseplate";
+		gm_LicensePlateSelectionsDigits[]=
+		{
+			"LicensePlate_01",
+			"LicensePlate_02",
+			"LicensePlate_03",
+			"LicensePlate_04",
+			"LicensePlate_05",
+			"LicensePlate_06",
+			"LicensePlate_07",
+			"LicensePlate_08"
+		};
+		gm_TacticalSignsDefaultIcon="";
+		gm_TacticalNumbersFontNumbers="";
+		gm_TacticalNumbersDefaultNumber="##";
+		gm_LicensePlateDefaultDigits="########";
+		crew="80s_tanoa_tucanoil_worker_blouse_base";
+        editorPreview="gm\gm_vehicles\gm_land\gm_wheeled\gm_ural375d\gm_gc_army_ural375d\data\ui\preview_gm_gc_army_ural375d_refuel.jpg";
+        class TransportItems
+        {
+            class _xx_burnBandage
+            {
+                name="gm_ge_army_burnBandage";
+                count=2;
+            };
+            class _xx_gauzeBandage
+            {
+                name="gm_ge_army_gauzeBandage";
+                count=2;
+            };
+            class _xx_gm_repairkit_01
+            {
+                name="gm_repairkit_01";
+                count=1;
+            };
+        };
+        class TransportWeapons
+        {
+            class _xx_binoculars
+            {
+                weapon="gm_df7x40_grn";
+                count=1;
+            };
+        };
+	};
+	class 80s_tanoa_tucanoil_ural_refuel: 80s_tanoa_tucanoil_ural_refuel_base
+	{
+        displayName="Ural (Fuel)";
+		scope=2;
+		scopeCurator=2;
+        scopeArsenal=2;
+		author="quarren";
+		hiddenSelectionsTextures[]=
+		{
+			"#(rgb,8,8,3)color(0.25,0.25,0.25,1,CO)",
+			"\80s_tanoa\80s_tanoa_vehicles\tucanoil\ural\data\80s_tanoa_tucanoil_ural_refuel_ext_01_co.paa",
+            "\80s_tanoa\80s_tanoa_vehicles\tucanoil\ural\data\80s_tanoa_tucanoil_ural_refuel_ext_01_co.paa",
+            "\80s_tanoa\80s_tanoa_vehicles\tucanoil\ural\data\80s_tanoa_tucanoil_ural_refuel_ext_03_co.paa",
+            "\80s_tanoa\80s_tanoa_vehicles\tucanoil\ural\data\80s_tanoa_tucanoil_ural_refuel_ext_01_co.paa",
+            "\80s_tanoa\80s_tanoa_vehicles\tucanoil\ural\data\80s_tanoa_tucanoil_ural_refuel_ext_01_co.paa"
+		};
+	};
+    // Tucan Oil (Civilian)
+    class 80s_tanoa_tucanoil_civ_ural_refuel: 80s_tanoa_tucanoil_ural_refuel
+    {
+        faction="80s_tanoa_fc_tucanoil_civ";
+        displayName="Ural (Fuel)";
+		side=3;
+        scope=2;
+        scopeArsenal=2;
+        crew="80s_tanoa_tucanoil_civ_worker_blouse_base";
+    };
+};
