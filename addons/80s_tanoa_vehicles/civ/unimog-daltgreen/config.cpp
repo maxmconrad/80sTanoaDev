c9e86480183219aeaa251d43e785411838f61fc4
Max-Morten Conrad <max@kokoni.eu>
2020-11-03 13:57:17 +0100

    Daltgreen unimog including textures

--- addons/80s_tanoa_vehicles/civ/unimog-daltgreen/config.cpp
+++ addons/80s_tanoa_vehicles/civ/unimog-daltgreen/config.cpp
@@ -4,7 +4,8 @@ class CfgPatches
 	{
 		units[]=
         {
-            "80s_tanoa_civ_unimog_daltgreen"
+            "80s_tanoa_civ_unimog_daltgreen",
+            "80s_tanoa_civ_unimog_daltgreen_expl"
         };
 		weapons[]=
         {
@@ -22,7 +23,7 @@ class CfgPatches
 class gm_texturesource_base;
 class CfgVehicles
 {
-    // Unarmed
+    // Unarmed Cover
     class gm_u1300l_container_base;
     class gm_u1300l_container_civ_base: gm_u1300l_container_base
 	{
@@ -41,7 +42,7 @@ class CfgVehicles
 					"\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_01_co.paa",
                     "\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_02_co.paa",
                     "\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_02_co.paa",
-					"\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_02_co.paa"
+					"\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_04_co.paa"
 				};
 			};
 		};
@@ -67,12 +68,12 @@ class CfgVehicles
 		gm_TacticalNumbersFontNumbers="";
 		gm_TacticalNumbersDefaultNumber="##";
 		gm_LicensePlateDefaultDigits="########";
-		crew="80s_tanoa_civ_uniform_miner_grn";
+		crew="80s_tanoa_civ_miner_daltgreen_base";
         editorPreview="gm\gm_vehicles\gm_land\gm_wheeled\gm_u1300l\gm_ge_army_u1300l\data\ui\preview_gm_ge_army_u1300l_cargo.jpg";
 	};
 	class 80s_tanoa_civ_unimog_daltgreen: 80s_tanoa_civ_unimog_daltgreen_base
 	{
-        displayName="Unimog";
+        displayName="Unimog (Daltgreen)";
 		scope=2;
 		scopeCurator=2;
         scopeArsenal=2;
@@ -109,6 +110,96 @@ class CfgVehicles
 			"\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_01_co.paa",
             "\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_02_co.paa",
             "\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_02_co.paa",
+            "\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_04_co.paa"
+		};
+	};
+    // Exploration
+    class gm_u1300l_base;
+    class gm_u1300l_medic_base: gm_u1300l_base
+    {
+        class TextureSources
+		{
+			class 80s_tanoa_daltgreen_gry: gm_texturesource_base
+			{
+				factions[]=
+				{
+					"80s_tanoa_fc_civ"
+				};
+				displayname="Daltgreen (Grey & Green)";
+				textures[]=
+				{
+					"#(rgb,8,8,3)color(0.25,0.25,0.25,1,CO)",
+					"\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_01_co.paa",
+                    "\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_02_co.paa",
+                    "\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_03_co.paa",
+					"\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_02_co.paa"
+				};
+			};
+		};
+    };
+    class 80s_tanoa_civ_unimog_daltgreen_expl_base: gm_u1300l_medic_base
+	{
+		side=3;
+		faction="80s_tanoa_fc_civ";
+		gm_InsigniasDefaultNation="80s_tanoa_insignia_daltgreen_01";
+		gm_LicensePlate="80s_tanoa_licenseplate";
+		gm_LicensePlateSelectionsDigits[]=
+		{
+			"LicensePlate_01",
+			"LicensePlate_02",
+			"LicensePlate_03",
+			"LicensePlate_04",
+			"LicensePlate_05",
+			"LicensePlate_06",
+			"LicensePlate_07",
+			"LicensePlate_08"
+		};
+		gm_TacticalSignsDefaultIcon="";
+		gm_TacticalNumbersFontNumbers="";
+		gm_TacticalNumbersDefaultNumber="##";
+		gm_LicensePlateDefaultDigits="########";
+		crew="80s_tanoa_civ_miner_daltgreen_base";
+        editorPreview="gm\gm_vehicles\gm_land\gm_wheeled\gm_u1300l\gm_ge_army_u1300l\data\ui\preview_gm_ge_army_u1300l_medic.jpg";
+	};
+	class 80s_tanoa_civ_unimog_daltgreen_expl: 80s_tanoa_civ_unimog_daltgreen_expl_base
+	{
+        displayName="Unimog (Daltgreen, Exploration)";
+		scope=2;
+		scopeCurator=2;
+        scopeArsenal=2;
+		author="quarren";
+        class TransportItems
+        {
+            class _xx_burnBandage
+            {
+                name="gm_ge_army_burnBandage";
+                count=2;
+            };
+            class _xx_gauzeBandage
+            {
+                name="gm_ge_army_gauzeBandage";
+                count=2;
+            };
+            class _xx_gm_repairkit_01
+            {
+                name="gm_repairkit_01";
+                count=1;
+            };
+        };
+        class TransportWeapons
+        {
+            class _xx_binoculars
+            {
+                weapon="gm_df7x40_grn";
+                count=1;
+            };
+        };
+		hiddenSelectionsTextures[]=
+		{
+			"#(rgb,8,8,3)color(0.25,0.25,0.25,1,CO)",
+			"\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_01_co.paa",
+            "\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_02_co.paa",
+            "\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_03_co.paa",
             "\80s_tanoa\80s_tanoa_vehicles\civ\unimog-daltgreen\data\80s_tanoa_civ_daltgreen_unimog_ext_02_co.paa"
 		};
 	};
