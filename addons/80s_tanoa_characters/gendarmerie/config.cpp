6daf68dc56e82e77c294a30acbb675832d7c8834
Max-Morten Conrad <max@kokoni.eu>
2020-11-09 14:13:37 +0100

    gendarmerie SIU dudes

--- addons/80s_tanoa_characters/gendarmerie/config.cpp
+++ addons/80s_tanoa_characters/gendarmerie/config.cpp
@@ -42,6 +42,48 @@ class CfgWeapons
             uniformClass="80s_tanoa_gendarme_base";
         };
 	};
+    // SIU 1
+    class 80s_tanoa_gendarmerie_uniform_blouse_brn: gm_ge_pol_uniform_blouse_80_blk
+	{
+		scope=2;
+        scopeArsenal=2;
+		displayName="SIU Clothes (Jacket)";
+        picture="\gm\gm_characters\gm_ge_characters\data\ui\picture_gm_ge_pol_uniform_blouse_80_blk_ca";
+        class ItemInfo: ItemInfo
+        {
+            uniformClass="80s_tanoa_gendarmerie_siu_blouse_brn_p1";
+            containerClass="Supply40";
+        };
+        hiddenSelectionsTextures[]=
+		{
+            "\80s_tanoa\80s_tanoa_characters\civ\data\80s_tanoa_civ_outfit_blouse_brn_co.paa",
+            "",
+            "",
+            ""
+		};
+	};
+    // SIU 2
+    class Uniform_Base
+    class U_C_Man_casual_2_F: Uniform_Base
+    {
+        class ItemInfo;
+    };
+    class 80s_tanoa_gendarmerie_uniform_casual_hawaiian_blk: U_C_Man_casual_2_F
+    {
+        scope=2;
+        scopeArsenal=2;
+		displayName="SIU Clothes (Casual)";
+        picture="\A3\characters_f_exp\Civil\data\ui\icon_U_C_Man_casual_2_F_ca.paa";
+        class ItemInfo: ItemInfo
+        {
+            uniformClass="80s_tanoa_gendarmerie_siu_casual_hawaiian_blk_mp5";
+            containerClass="Supply40";
+        };
+        hiddenSelectionsTextures[]=
+		{
+            "\80s_tanoa\80s_tanoa_characters\civ\data\80s_tanoa_civ_outfit_casual_hawaiian_co.paa"
+		};
+    };
     class gm_ge_vest_80_mp_base;
     class 80s_tanoa_gendarmerie_vest: gm_ge_vest_80_mp_base
     {
@@ -345,6 +387,191 @@ class CfgVehicles
             "80s_tanoa_gendarmerie_vest"
 		};
 	};
+    class 80s_tanoa_gendarmerie_siu_blouse_brn_p1: 80s_tanoa_gendarme_base
+    {
+        displayName="Agent (P1)";
+        scope=2;
+        scopeArsenal=2;
+        uniformClass="80s_tanoa_gendarmerie_uniform_blouse_brn";
+        weapons[]=
+		{
+			"Throw",
+			"Put",
+            "gm_p1_blk"
+		};
+		respawnWeapons[]=
+		{
+			"Throw",
+			"Put",
+            "gm_p1_blk"
+		};
+        magazines[]=
+		{
+			"gm_8rnd_9x19mm_b_dm11_p1_blk",
+            "gm_8rnd_9x19mm_b_dm11_p1_blk",
+            "gm_8rnd_9x19mm_b_dm11_p1_blk",
+            "gm_8rnd_9x19mm_b_dm11_p1_blk",
+            "gm_8rnd_9x19mm_b_dm11_p1_blk"
+		};
+		respawnMagazines[]=
+		{
+			"gm_8rnd_9x19mm_b_dm11_p1_blk",
+            "gm_8rnd_9x19mm_b_dm11_p1_blk",
+            "gm_8rnd_9x19mm_b_dm11_p1_blk",
+            "gm_8rnd_9x19mm_b_dm11_p1_blk",
+            "gm_8rnd_9x19mm_b_dm11_p1_blk"
+		};
+        linkedItems[]=
+		{
+			"gm_gc_compass_f73",
+			"ItemRadio",
+            "itemMap",
+            "gm_watch_kosei_80",
+            "G_Spectacles_tinted"
+		};
+		respawnLinkedItems[]=
+		{
+			"gm_gc_compass_f73",
+			"ItemRadio",
+            "itemMap",
+            "gm_watch_kosei_80",
+            "G_Spectacles_tinted"
+		};
+        hiddenSelectionsTextures[]=
+		{
+            "\80s_tanoa\80s_tanoa_characters\civ\data\80s_tanoa_civ_outfit_blouse_brn_co.paa",
+            "",
+            "",
+            ""
+		};
+    };
+    class 80s_tanoa_gendarmerie_siu_blouse_brn_mp5: 80s_tanoa_gendarmerie_siu_blouse_brn_p1
+    {
+        displayName="Agent (Blouse, MP5)";
+        scope=2;
+        scopeArsenal=2;
+        weapons[]=
+		{
+			"Throw",
+			"Put",
+            "gm_mp5a3_blk"
+		};
+		respawnWeapons[]=
+		{
+			"Throw",
+			"Put",
+            "gm_mp5a3_blk"
+		};
+        magazines[]=
+		{
+			"gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+			"gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+			"gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+			"gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+            "gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+            "gm_30rnd_9x19mm_b_dm51_mp5a3_blk"
+		};
+		respawnMagazines[]=
+		{
+			"gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+			"gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+			"gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+			"gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+            "gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+            "gm_30rnd_9x19mm_b_dm51_mp5a3_blk"
+		};
+        linkedItems[]=
+		{
+			"gm_gc_compass_f73",
+			"ItemRadio",
+            "itemMap",
+            "gm_watch_kosei_80",
+            "G_Spectacles_tinted"
+		};
+		respawnLinkedItems[]=
+		{
+			"gm_gc_compass_f73",
+			"ItemRadio",
+            "itemMap",
+            "gm_watch_kosei_80",
+            "G_Spectacles_tinted"
+		};
+    };
+    class C_Man_casual_2_F;
+    class 80s_tanoa_civ_man_casual_hawaiian_blk: C_Man_casual_2_F
+    {
+        displayName="Agent (MP5)";
+        uniformClass="80s_tanoa_gendarmerie_uniform_casual_hawaiian_blk";
+        faction="80s_tanoa_gendarmerie";
+		rankInsignias="army_wht";
+        faceType="Man_A3";
+		side=1;
+		genericNames="TanoanMen";
+		vehicleClass="Men";
+		editorSubcategory="gm_esc_men_80";
+        scope=2;
+        scopeArsenal=2;
+		isgmContent=1;
+		dlc="gm";
+        author="quarren";
+		weapons[]=
+		{
+			"Throw",
+			"Put",
+            "gm_mp5a3_blk"
+		};
+		respawnWeapons[]=
+		{
+			"Throw",
+			"Put",
+            "gm_mp5a3_blk"
+		};
+        magazines[]=
+		{
+			"gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+			"gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+			"gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+			"gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+            "gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+            "gm_30rnd_9x19mm_b_dm51_mp5a3_blk"
+		};
+		respawnMagazines[]=
+		{
+			"gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+			"gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+			"gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+			"gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+            "gm_30rnd_9x19mm_b_dm51_mp5a3_blk",
+            "gm_30rnd_9x19mm_b_dm51_mp5a3_blk"
+		};
+		linkedItems[]=
+		{
+			"gm_gc_compass_f73",
+			"ItemRadio",
+            "itemMap",
+            "gm_watch_kosei_80",
+            "80s_tanoa_hia_specops_boonie",
+            "gm_ge_bgs_vest_80_rifleman"
+		};
+		respawnLinkedItems[]=
+		{
+			"gm_gc_compass_f73",
+			"ItemRadio",
+            "itemMap",
+            "gm_watch_kosei_80",
+            "80s_tanoa_hia_specops_boonie",
+            "gm_ge_bgs_vest_80_rifleman"
+		};
+		identityTypes[]=
+		{
+			"LanguageENGFRE_F",
+			"Head_Tanoan",
+		};
+        hiddenSelectionsTextures[]=
+		{
+            "\80s_tanoa\80s_tanoa_characters\civ\data\80s_tanoa_civ_outfit_casual_hawaiian_co.paa"
+		};
+    };
 };
 class CfgGroups
 {
